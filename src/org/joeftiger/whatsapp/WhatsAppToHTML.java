1af02156cac02b306a927a1dec055f61193cb25b
j.oeftiger <j.oeftiger@protonmail.com>
2019-11-06 14:32:51 +0100

    Update WhatsAppToHTML.java

--- src/org/joeftiger/whatsapp/WhatsAppToHTML.java
+++ src/org/joeftiger/whatsapp/WhatsAppToHTML.java
@@ -18,31 +18,41 @@ public class WhatsAppToHTML {
 		creator.addMessagesFrom(parser);
 
 		System.out.println("Saving ...");
-		save();
-	}
-
-	private void init(String[] args) throws IOException {
-		if (args.length != 2 || args[0].equals("-h") || args[0].equals("--help")) {
-			showHelp();
-			System.exit(-1);
-		}
-
-		outgoingUser = args[0];
-		filePath = Path.of(args[1]);
-		creator = new DocumentCreator(Path.of("./res/index.html"));
-
-		if (outgoingUser == null || outgoingUser.isBlank() || filePath == null) {
-			showHelp();
-			System.exit(-1);
-		}
-	}
-
-	private void save() {
 		createDirectory();
 		saveCSS();
 		saveHTML();
 	}
 
+	/**
+	 * Initializes the file path etc. from the given arguments.
+	 *
+	 * @param args program arguments
+	 */
+	private void init(String[] args) {
+		if (args.length != 2) {
+			showHelp();
+			System.exit(1);
+		}
+
+		outgoingUser = args[0];
+		filePath = Path.of(args[1]);
+		try {
+			creator = new DocumentCreator(Path.of("./res/index.html"));
+		} catch (IOException e) {
+			printError("Program error: Can't create the document from the base html.");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		if (outgoingUser == null || outgoingUser.isBlank() || filePath == null) {
+			showHelp();
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * Creates the directory {@code <base>/html/}.
+	 */
 	private void createDirectory() {
 		String directory = filePath.getParent() + "/html";
 		System.out.println("Creating directory:\t[" + directory + "] ...");
@@ -56,6 +66,9 @@ public class WhatsAppToHTML {
 		}
 	}
 
+	/**
+	 * Copies the stylesheet file to {@code <base>/html/style.css}.
+	 */
 	private void saveCSS() {
 		String css = filePath.getParent() + "/html/style.css";
 		System.out.println("Writing CSS:\t\t[" + css + "] ...");
@@ -67,6 +80,9 @@ public class WhatsAppToHTML {
 		}
 	}
 
+	/**
+	 * Writes the created html file to {@code <base>/html/index.html}.
+	 */
 	private void saveHTML() {
 		String html = filePath.getParent() + "/html/index.html";
 		System.out.println("Writing HTML:\t\t[" + html + "] ...");
@@ -78,12 +94,20 @@ public class WhatsAppToHTML {
 		}
 	}
 
+	/**
+	 * Prints the given messages in error format.
+	 *
+	 * @param errors error messages
+	 */
 	private void printError(String... errors) {
 		for (String e : errors) {
 			System.err.println("-- " + e);
 		}
 	}
 
+	/**
+	 * Prints basic help for usage of this program.
+	 */
 	private void showHelp() {
 		System.err.println("usage:\t<outgoing user> <path to chat.txt>");
 	}
